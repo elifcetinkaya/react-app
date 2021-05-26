b55c86e08dcaea5b6ef5e5d2bb51465278bdc10e
elifcetinkaya <eliifctnk@gmail.com>
2021-05-26 16:29:49 +0300

    state and setState

--- react-app/src/components/Collapse.js
+++ react-app/src/components/Collapse.js
@@ -1,25 +1,49 @@
+// İlk olarak constructor --> state initialization
+// render methodu ilk defa çalıştı
+// onclick event'i çalıştı.
+// setState state nesnesini güncelledi. Triggered render
+// render methodu güncel JSX'i sayfada gösterdi.
+
 import React from "react";
 
-const Collapse = (props) => {
-  console.log(props.children);
-  return (
-    <div>
-      <p>
-        <a
-          className="btn btn-primary w-100"
-          data-bs-toggle="collapse"
-          href={"#".concat(props.href)}
-          role="button"
-          aria-expanded="false"
-          aria-controls="collapseExample"
-        >
+class Collapse extends React.Component {
+  constructor() {
+    super(); //React methodu içindeki constructor methodunu almayı sağlar.
+
+    this.state = {
+      showContent: false
+    }
+
+    //this.showMore = this.showMore.bind(this)
+
+  }
+
+  showMore = () =>{
+    this.setState({showContent: !this.state.showContent})
+  }
+
+  /*showMore(){
+    this.setState({showContent: true})
+    //console.log(this)
+    
+  }*/
+
+
+  render() {
+    console.log(this)
+    return (
+      <div>
+        <button className="btn btn-primary w-100" onClick={this.showMore}>
           Link with href
-        </a>
-      </p>
-      <div className="collapse show" id={props.href}>
-        {props.children}
+        </button>
+        {this.state.showContent ? (
+          <div className="collapse show">
+            {this.props.children}
+            {this.state.showContent}
+          </div>
+        ) : null}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Collapse;
