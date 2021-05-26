07e6cff0c4b84a63357c87c571ed0ccbe8d01ed9
elifcetinkaya <eliifctnk@gmail.com>
2021-05-26 18:49:44 +0300

    state vs props, lifecycle methods

--- react-app/src/components/Collapse.js
+++ react-app/src/components/Collapse.js
@@ -7,38 +7,32 @@
 import React from "react";
 
 class Collapse extends React.Component {
-  constructor() {
-    super(); //React methodu içindeki constructor methodunu almayı sağlar.
 
-    this.state = {
-      showContent: false
-    }
+  state = {showContent: false}
 
-    //this.showMore = this.showMore.bind(this)
+  showMore = () => {
+    this.setState({ showContent: !this.state.showContent });
+  };
 
-  }
+  // componentDidMount(){
+  //   console.log('Component oluşturuldu.')
+  // }
 
-  showMore = () =>{
-    this.setState({showContent: !this.state.showContent})
-  }
-
-  /*showMore(){
-    this.setState({showContent: true})
-    //console.log(this)
-    
-  }*/
-
+  // componentDidUpdate(){
+  //   console.log('Component güncellendi.')
+  // }
 
   render() {
-    console.log(this)
     return (
       <div>
         <button className="btn btn-primary w-100" onClick={this.showMore}>
-          Link with href
+          {/* {this.props.children.props.cardTitle} */}
+          {React.Children.map(this.props.children, children => children.props.cardTitle)}
         </button>
         {this.state.showContent ? (
           <div className="collapse show">
-            {this.props.children}
+            {/* {this.props.children} */}
+            {React.Children.map(this.props.children, children => children)}
             {this.state.showContent}
           </div>
         ) : null}
